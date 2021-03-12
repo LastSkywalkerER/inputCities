7aec66fdfaf891f8ad0b023e3983573990617b03
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-12 18:12:18 +0300

    v1.0.2 first render default list

--- src/index.js
+++ src/index.js
@@ -4,7 +4,10 @@
 const selectCities = document.getElementById('select-cities'),
   listDefault = document.querySelector('.dropdown-lists__list--default'),
   listSelect = document.querySelector('.dropdown-lists__list--select'),
-  listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete');
+  listAutocomplete = document.querySelector('.dropdown-lists__list--autocomplete'),
+  urlData = 'http://localhost:3000/RU';
+
+const getData = () => fetch(urlData);
 
 const hideAll = () => {
   listDefault.style.display = 'none';
@@ -32,7 +35,7 @@ const listeners = () => {
   document.addEventListener('click', event => {
     if (event.target === selectCities) {
       showDefault();
-    } else if (event.target.closest(listDefault.classList) || event.target.closest(listSelect.classList) || event.target.closest(listAutocomplete.classList)) {
+    } else if (!event.target.closest('.' + listDefault.classList[1]) && !event.target.closest('.' + listSelect.classList[1]) && !event.target.closest('.' + listAutocomplete.classList[1])) {
       hideAll();
     }
   });
@@ -40,8 +43,46 @@ const listeners = () => {
   selectCities.addEventListener('input', () => {
     showSelect();
   });
+};
 
+const bubleSort = (array, property) => {
+  for (let i in array) {
+    for (let j = 1; j < array.length - i; j++) {
+      if (+array[j - 1][property] < +array[j][property]) {
+        const temp = array[j - 1];
+        array[j - 1] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+const renderCounry = (listForInsert, country) => {
+  const countryBlock = document.createElement('div');
+  countryBlock.classList.add('dropdown-lists__countryBlock');
+  countryBlock.innerHTML = `
+  <div class="dropdown-lists__total-line">
+    <div class="dropdown-lists__country">${country.country}</div>
+    <div class="dropdown-lists__count">${country.count}</div>
+  </div>
+  `;
+
+  listForInsert.append(countryBlock);
+};
+
+const renderCity = (city) => {};
+
+const renderDefaultList = () => {
+  listDefault.childNodes[1].textContent = '';
+  getData()
+    .then(request => request.json())
+    .then(request => {
+      request.map(country => bubleSort(country.cities, 'count'));
+      request.forEach(country => renderCounry(listDefault.childNodes[1], country));
+    });
 };
 
 hideAll();
 listeners();
+renderDefaultList();
