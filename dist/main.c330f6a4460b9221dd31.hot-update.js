1a009c0aaa9c050ac75afbb76b647b3e2b4b88c8
LastSkywalkerER <maxdr1998@gmail.com>
2021-03-13 13:14:37 +0300

    v1.2.0 (animation)

---
+++ dist/main.c330f6a4460b9221dd31.hot-update.js
@@ -0,0 +1,31 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdategloJS2"]("main",{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\src\\\\index.js: Missing semicolon (188:43)\\n\\n\\u001b[0m \\u001b[90m 186 |\\u001b[39m       }\\u001b[0m\\n\\u001b[0m \\u001b[90m 187 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 188 |\\u001b[39m       console\\u001b[33m.\\u001b[39mlog(closeButton\\u001b[33m.\\u001b[39mclassList[\\u001b[35m0\\u001b[39m]))\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m     |\\u001b[39m                                            \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 189 |\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 190 |\\u001b[39m     \\u001b[36mif\\u001b[39m (event\\u001b[33m.\\u001b[39mtarget\\u001b[33m.\\u001b[39mclosest(closeButton\\u001b[33m.\\u001b[39mclassList[\\u001b[35m0\\u001b[39m])) {\\u001b[0m\\n\\u001b[0m \\u001b[90m 191 |\\u001b[39m       selectCities\\u001b[33m.\\u001b[39mvalue \\u001b[33m=\\u001b[39m \\u001b[32m''\\u001b[39m\\u001b[33m;\\u001b[39m\\u001b[0m\\n    at Parser._raise (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:776:17)\\n    at Parser.raiseWithData (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:769:17)\\n    at Parser.raise (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:737:17)\\n    at Parser.semicolon (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:9668:10)\\n    at Parser.parseExpressionStatement (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12740:10)\\n    at Parser.parseStatementContent (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12338:19)\\n    at Parser.parseStatement (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12202:17)\\n    at Parser.parseBlockOrModuleBlockBody (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12782:25)\\n    at Parser.parseBlockBody (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12773:10)\\n    at Parser.parseBlock (C:\\\\MAMP\\\\htdocs\\\\inputCities\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12757:10)\");\n\n//# sourceURL=webpack://gloJS2/./src/index.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ 	"use strict";
+/******/ 
+/******/ 	/* webpack/runtime/getFullHash */
+/******/ 	(() => {
+/******/ 		__webpack_require__.h = () => ("e16426e95b2825e8488d")
+/******/ 	})();
+/******/ 	
+/******/ }
+);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable max-len */
 'use strict';
 
+// Определяем все элементы страницы и переменные для отображения состояния выпадающего списка
 const selectCities = document.getElementById('select-cities'),
   listDefault = document.querySelector('.dropdown-lists__list--default'),
   listSelect = document.querySelector('.dropdown-lists__list--select'),
@@ -8,57 +9,164 @@ const selectCities = document.getElementById('select-cities'),
   closeButton = document.querySelector('.close-button'),
   button = document.querySelector('.button'),
   label = document.querySelector('label'),
+  mainWindow = document.querySelector('.input-cities'),
   urlData = 'http://localhost:3000/RU',
   countryLineSelector = '.dropdown-lists__total-line',
-  cityLineSelector = '.dropdown-lists__line';
-let listState = '';
+  cityLineSelector = '.dropdown-lists__line',
+  spiner = document.createElement('div'),
+  mainBlockWidth = mainWindow.clientWidth;
+let listState = '',
+  appData = [];
 
+// анимация загрузки данных
+const loadingPage = () => {
+  spiner.classList.add('sk-circle-bounce');
+  for (let i = 1; i < 13; i++) {
+    spiner.insertAdjacentHTML('beforeend', `<div class='sk-child sk-circle-${i}'></div>`);
+  }
+
+  mainWindow.style.display = 'none';
+  mainWindow.insertAdjacentElement('afterend', spiner);
+};
+
+// отображение элементов после загрузки данных
+const loadedPgae = () => {
+  spiner.remove();
+  mainWindow.style.display = 'block';
+};
+
+// зпрос на получение данных с сервера
 const getData = callback => fetch(urlData)
   .then(request => request.json())
   .then(request => callback(request));
 
+// создание контейнера для страны с городами и отрисовка строки с этой страной
+const renderCounry = (listForInsert, country) => {
+  const countryBlock = document.createElement('div');
+  countryBlock.classList.add('dropdown-lists__countryBlock');
+  countryBlock.innerHTML = `
+    <div class="dropdown-lists__total-line">
+      <div class="dropdown-lists__country">${country.country}</div>
+      <div class="dropdown-lists__count">${country.count}</div>
+    </div>
+    `;
+
+  listForInsert.append(countryBlock);
+};
+
+// отрисовка городов в быранной стране
+const renderCity = (listForInsert, city, matchReg, input = '') => {
+  const cityLine = document.createElement('div');
+  cityLine.classList.add('dropdown-lists__line');
+  cityLine.innerHTML = `
+    <div class="dropdown-lists__city">
+      <strong>${input.slice(0, 1).toUpperCase() + input.slice(1).toLowerCase()}</strong>${city.name.replace(matchReg, '')}
+    </div>
+    <div class="dropdown-lists__count">${city.count}</div>
+    `;
+
+  listForInsert.append(cityLine);
+};
+
+// отрисовка стандартного списка со странами и ТОП-3 городами
+const renderDefaultList = () => {
+  const insertPoint = listDefault.childNodes[1];
+  insertPoint.textContent = '';
+
+  appData.forEach(country => {
+    renderCounry(insertPoint, country);
+    for (let i = 0; i < 3; i++) {
+      renderCity(insertPoint, country.cities[i]);
+    }
+  });
+  console.log(appData);
+
+};
+
+// отрисовка списка с выбранной страной и всеми её городами
+const renderSelectList = countryName => {
+  const insertPoint = listSelect.childNodes[1];
+  insertPoint.textContent = '';
+  appData.forEach(country => {
+    if (country.country === countryName) {
+      renderCounry(insertPoint, country);
+      country.cities.forEach(city => {
+        renderCity(insertPoint, city);
+      });
+    }
+  });
+};
+
+// отрисовка списка городов совпадающих с введёнными буквами
+const renderAutocompleteList = input => {
+  const insertPoint = listAutocomplete.childNodes[1],
+    searchingRegular = new RegExp(`^${input}`, `gi`);
+  insertPoint.textContent = '';
+  appData.forEach(country => {
+    country.cities.forEach(city => {
+      if (searchingRegular.test(city.name)) {
+        button.href = city.link;
+        renderCity(insertPoint, city, searchingRegular, input);
+      }
+    });
+  });
+  if (!insertPoint.textContent) {
+    renderCity(insertPoint, {
+      name: 'Ничего не найдено',
+      count: ''
+    });
+  }
+};
+
+// прячем все списки и чистим состояния
 const hideAll = () => {
-  button.setAttribute('disabled', '');
-  button.href = '#';
+  // button.setAttribute('disabled', '');
+  // button.href = '#';
   label.style.display = 'block';
-  closeButton.style.display = 'none';
+  // closeButton.style.display = 'none';
   listState = '';
   listDefault.style.display = 'none';
   listSelect.style.display = 'none';
   listAutocomplete.style.display = 'none';
 };
 
+// показываем стандартный список, чистим кнопку и инпут
 const showDefault = () => {
   button.setAttribute('disabled', '');
   button.href = '#';
-  label.style.display = 'none';
-  closeButton.style.display = 'none';
-  listState = 'default';
+  selectCities.value = '';
+  // label.style.display = 'none';
+  // closeButton.style.display = 'none';
   listDefault.style.display = 'block';
   listSelect.style.display = 'none';
   listAutocomplete.style.display = 'none';
 };
-const showSelect = () => {
+
+// показываем список выбранной страны, чистим кнопку, передаём в инпут выбранную страну
+const showSelect = event => {
   button.setAttribute('disabled', '');
   button.href = '#';
+  const clickText = event.target.closest(countryLineSelector).childNodes[1].textContent.trim();
+  selectCities.value = clickText;
+  renderSelectList(clickText);
   label.style.display = 'none';
   closeButton.style.display = 'block';
-  listState = 'select';
   listDefault.style.display = 'none';
   listSelect.style.display = 'block';
   listAutocomplete.style.display = 'none';
 };
+
+// показываем список с автозаполнением, меняем сосотояние
 const showAutocomplete = () => {
-  label.style.display = 'none';
-  closeButton.style.display = 'block';
+  // label.style.display = 'none';
+  // closeButton.style.display = 'block';
   listState = 'autocomplete';
   listDefault.style.display = 'none';
   listSelect.style.display = 'none';
   listAutocomplete.style.display = 'block';
 };
 
-
-
+// сортировка пузырьком по убыванию и по свойству
 const bubleSort = (array, property) => {
   for (let i in array) {
     for (let j = 1; j < array.length - i; j++) {
@@ -72,103 +180,81 @@ const bubleSort = (array, property) => {
   return array;
 };
 
-const renderCounry = (listForInsert, country) => {
-  const countryBlock = document.createElement('div');
-  countryBlock.classList.add('dropdown-lists__countryBlock');
-  countryBlock.innerHTML = `
-  <div class="dropdown-lists__total-line">
-    <div class="dropdown-lists__country">${country.country}</div>
-    <div class="dropdown-lists__count">${country.count}</div>
-  </div>
-  `;
+// тригер анимации
+const toggleAnimation = event => {
+  let position = 0;
 
-  listForInsert.append(countryBlock);
-};
+  mainWindow.childNodes[9].style.overflowX = 'hidden';
+  mainWindow.childNodes[9].childNodes[1].style.width = `${mainBlockWidth * 2}px`;
+  mainWindow.childNodes[9].childNodes[1].style.position = 'relative';
 
-const renderCity = (listForInsert, city) => {
-  const cityLine = document.createElement('div');
-  cityLine.classList.add('dropdown-lists__line');
-  cityLine.innerHTML = `
-  <div class="dropdown-lists__city">
-    ${city.name}
-  </div>
-  <div class="dropdown-lists__count">${city.count}</div>
-  `;
+  //анимация от стандартного к выбору
+  const animationDefault = () => {
+    showSelect(event);
+    listDefault.style.display = 'block';
 
-  listForInsert.append(cityLine);
-};
+    position += 50;
+    mainWindow.childNodes[9].childNodes[1].style.right = `${position}px`;
 
-const renderDefaultList = () => {
-  const insertPoint = listDefault.childNodes[1];
-  insertPoint.textContent = '';
-  getData(request => {
-    request.map(country => bubleSort(country.cities, 'count'));
-    request.forEach(country => {
-      renderCounry(insertPoint, country);
-      for (let i = 0; i < 3; i++) {
-        renderCity(insertPoint, country.cities[i]);
-      }
-    });
-    console.log(request);
-  });
-};
-
-const renderSelectList = countryName => {
-  const insertPoint = listSelect.childNodes[1];
-  insertPoint.textContent = '';
-  getData(request => {
-    request.map(country => bubleSort(country.cities, 'count'));
-    request.forEach(country => {
-      if (country.country === countryName) {
-        renderCounry(insertPoint, country);
-        country.cities.forEach(city => {
-          renderCity(insertPoint, city);
-        });
-      }
-    });
-  });
-};
-
-const renderAutocompleteList = input => {
-  const insertPoint = listAutocomplete.childNodes[1],
-    searchingRegular = new RegExp(`^${input}`, `i`);
-  insertPoint.textContent = '';
-  getData(request => {
-    request.map(country => bubleSort(country.cities, 'count'));
-    request.forEach(country => {
-      country.cities.forEach(city => {
-        if (searchingRegular.test(city.name)) {
-          button.href = city.link;
-          renderCity(insertPoint, city);
-        }
-      });
-    });
-    if (!insertPoint.textContent) {
-      renderCity(insertPoint, {
-        name: 'Ничего не найдено',
-        count: ''
-      });
+    if (position <= mainBlockWidth) {
+      requestAnimationFrame(animationDefault);
+      return;
     }
-  });
+
+    showSelect(event);
+
+    mainWindow.childNodes[9].childNodes[1].style.width = `100%`;
+    mainWindow.childNodes[9].childNodes[1].style.position = 'static';
+  };
+
+  //анимация от выбора к стандартному
+  const animationSelect = () => {
+    showDefault();
+    listSelect.style.display = 'block';
+
+    position -= 50;
+    mainWindow.childNodes[9].childNodes[1].style.right = `${position}px`;
+
+    if (position >= 0) {
+      requestAnimationFrame(animationSelect);
+      return;
+    }
+
+    showDefault(event);
+
+    mainWindow.childNodes[9].childNodes[1].style.width = `100%`;
+    mainWindow.childNodes[9].childNodes[1].style.position = 'static';
+  };
+
+  if (listState === 'default') {
+    position = 0;
+    requestAnimationFrame(animationDefault);
+    listState = 'select';
+  } else if (listState === 'select') {
+    position = mainBlockWidth;
+    requestAnimationFrame(animationSelect);
+    listState = 'default';
+  }
+
 };
 
+// вешаем события
 const listeners = () => {
+  // проверяем клик
   document.addEventListener('click', event => {
-    if (event.target === selectCities && selectCities.value === '') {
+    // клик по инпуту - показываем стандартный список, если мимо прячем всё
+    if (event.target === selectCities) {
       showDefault();
+      listState = 'default';
     } else if (!event.target.closest('.' + listDefault.classList[1]) && !event.target.closest('.' + listSelect.classList[1]) && !event.target.closest('.' + listAutocomplete.classList[1]) && !event.target.closest('#' + selectCities.id) && !event.target.closest('.' + button.classList[0])) {
-      selectCities.value = '';
+      // selectCities.value = '';
       hideAll();
     }
-    if (listState === 'default' && event.target.closest(countryLineSelector)) {
-      const clickText = event.target.closest(countryLineSelector).childNodes[1].textContent.trim();
-      selectCities.value = clickText;
-      renderSelectList(clickText);
-      showSelect();
-    } else if (listState === 'select' && event.target.closest(countryLineSelector)) {
-      selectCities.value = '';
-      showDefault();
+    // клик по стране, переключение списков между стандартным и выбором городов
+    if (event.target.closest(countryLineSelector)) {
+      toggleAnimation(event);
     }
+    // клик по городу - заполнем инпут и кнопку
     if (event.target.closest(cityLineSelector)) {
       const clickText = event.target.closest(cityLineSelector).childNodes[1].textContent.trim();
       selectCities.value = clickText;
@@ -176,6 +262,22 @@ const listeners = () => {
       renderAutocompleteList(selectCities.value.trim());
       showAutocomplete();
     }
+    // убираем бесящий лейбл если поле нее пустое при кликах
+    if (selectCities.value) {
+      label.style.display = 'none';
+      closeButton.style.display = 'block';
+    } else {
+      label.style.display = 'block';
+      closeButton.style.display = 'none';
+    }
+    // клик по кресту чистит всё
+    if (event.target.closest('.' + closeButton.classList[0])) {
+      selectCities.value = '';
+      hideAll();
+      closeButton.style.display = 'none';
+    }
+
+
   });
 
   selectCities.addEventListener('input', () => {
@@ -184,10 +286,28 @@ const listeners = () => {
       showAutocomplete();
     } else {
       showDefault();
+      listState = 'default';
     }
+
+    if (selectCities.value) {
+      label.style.display = 'none';
+      closeButton.style.display = 'block';
+    } else {
+      label.style.display = 'block';
+      closeButton.style.display = 'none';
+    }
+
+
   });
 };
 
-hideAll();
-listeners();
-renderDefaultList();
+// грузим данные и по загрузке рисуем элементы
+loadingPage();
+getData(request => {
+  request.map(country => bubleSort(country.cities, 'count'));
+  appData = request;
+  loadedPgae();
+  hideAll();
+  listeners();
+  renderDefaultList();
+});
